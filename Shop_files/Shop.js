ea7fecf4caa00c7c1321e534e989c997afffcf8b
Ed Preston <epreston@prestonsoft.com>
2015-04-12 01:10:21 +1000

    Remove unused Shop_files directory and media
    We have replaced shop with an actual shop.  These files are no longer
    required.

--- Shop_files/Shop.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20141029
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Shop_files/ShopMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');fixupIECSS3Opacity('id2');applyEffects()}
